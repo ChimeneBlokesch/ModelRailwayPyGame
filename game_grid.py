65cb0829f98550cee656742080f8358dec490d25
Chimène Blokesch <chimene.blokesch2002@gmail.com>
2021-10-27 20:03:11 +0200

    added coordinate system

--- game_grid.py
+++ game_grid.py
@@ -3,6 +3,9 @@ import pygame
 
 GREEN = (0, 255, 0)
 
+pygame.init()
+FONT = pygame.font.Font(None, 15)
+
 
 class Grid:
     def __init__(self, screen: pygame.Surface, pos, treinen_db):
@@ -15,7 +18,18 @@ class Grid:
     def draw(self):
         self.screen.blit(self.grid, self.pos)
         self.grid.fill(GREEN)
+        self.show_coordinate_system()
         [train.draw() for train in self.trains]
+
+    def show_coordinate_system(self):
+        step = 50
+        for w in range(step, self.grid.get_size()[0], step):
+            text_surface = FONT.render(str(w), True, (0, 0, 0))
+            self.grid.blit(text_surface, (w, 2))
+
+        for h in range(step, self.grid.get_size()[0], step):
+            text_surface = FONT.render(str(h), True, (0, 0, 0))
+            self.grid.blit(text_surface, (2, h))
 
     def add_train(self, start_x, start_y, angle, filename):
         train = Train(self, start_x, start_y, angle, filename)
