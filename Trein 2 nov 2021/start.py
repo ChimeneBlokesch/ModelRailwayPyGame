e77111f3bb6c4f0ba690c553e38c9aab8f7ad155
ChimeneBlokesch <chimene.blokesch2002@gmail.com>
2025-09-12 16:37:00 +0200

    Added comments

--- Trein 2 nov 2021/start.py
+++ Trein 2 nov 2021/start.py
@@ -1,6 +1,8 @@
+# Inspiration for this code:
 # https://medium.com/@plaha.roshan/creating-a-simple-train-simulator-with-pygame-182204df7f04
 
 import pygame
+
 from commands import Commands
 from game_grid import Grid
 from database_treinen import Treinen
@@ -9,18 +11,21 @@ from database_treinen import Treinen
 if __name__ == "__main__":
     pygame.init()
     clock = pygame.time.Clock()
-    # flags = pygame.FULLSCREEN | pygame.RESIZABLE
+
     flags = 0
+
     screen = pygame.display.set_mode([1000, 600], flags)
-    pygame.display.set_caption("Treinen")
+    pygame.display.set_caption("Model Railway 2D")
     screen.fill((255, 255, 255))
-    # background = pygame.Surface(screen.get_size())
+
     loop = True
     command_field = Commands(screen)
     command_field.draw()
     treinen_db = Treinen()
     grid = Grid(screen, (0, command_field.field.size[1]), treinen_db)
-    grid.add_train(250, 50, 0, "Treinen/VIRM_kop_links.png")
+
+    # Initialize the grid with a train on an oval rails
+    grid.add_train(250, 50, 0, "trains/custom_side.png")
     grid.add_rails(0, 150, 50, 100, 100)
     grid.add_rails(1, 150, 50, 750, 50)
     grid.add_rails(0, 150, 150, 100, 100)
@@ -30,11 +35,13 @@ if __name__ == "__main__":
 
     command_field.grid = grid
 
+    # Keeps the game running
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
 
+            # Detect whether the command-line should be active or not
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if command_field.field.collidepoint(event.pos):
                     command_field.toggle()
@@ -42,6 +49,7 @@ if __name__ == "__main__":
                     command_field.active = False
 
             if event.type == pygame.KEYDOWN:
+                # Use the input character for the command
                 if command_field.active:
                     loop = command_field.typing(event)
 
